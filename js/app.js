8a5bcb8a694b8955bcc6c55b299c42379cff44d2
Eric Ochieng <ricochieng@gmail.com>
2018-03-07 12:05:21 +0300

    The registration form is now responsive and inputting data

--- js/app.js
+++ js/app.js
@@ -8,35 +8,60 @@ function User (first, last, id, phone, email, address, code) {
   this.address=address;
   this.postCode=code;
 }
+User.prototype.firstName = function() {
+  return this.firstName;
+}
+User.prototype.lastName = function() {
+  return this.lastName;
+}
+User.prototype.idNumber = function() {
+  return this.idNumber;
+}
+User.prototype.phoneNumber = function() {
+  return this.phoneNumber;
+}
+User.prototype.email = function() {
+  return this.email;
+}
+User.prototype.address = function() {
+  return this.address;
+}
+User.prototype.postCode = function() {
+  return this.postCode;
+}
 //User interface Logic
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+  $("#submit").click(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedIDNumber = $("input#new-id-number").val();
-    var inputtedPhoneNumber= $("input#new-phone-number").val();
-    var inputtedEmail = $("input#new-email").val();
-    var inputtedAddress = $("input#new-address").val();
-    var inputtedPostCode = $("input#new-post-code").val();
-
+    var inputtedFirstName = $("#new-first-name").val();
+    var inputtedLastName = $("#new-last-name").val();
+    var inputtedIDNumber = $("#new-id-number").val();
+    var inputtedPhoneNumber= $("#new-phone-number").val();
+    var inputtedEmail = $("#new-email").val();
+    var inputtedAddress = $("#new-address").val();
+    var inputtedPostCode = $("#new-post-code").val();
     var newUser = new User(inputtedFirstName, inputtedLastName, inputtedIDNumber, inputtedPhoneNumber, inputtedEmail, inputtedAddress, inputtedPostCode);
-    $("p#show-user").append("<p><span class='new-first-name'>" + newUser.firstName + "</span></p>");
-    $("p#show-user").append("<p><span class='new-last-name'>" + newUser.lastName + "</span></p>");
-    $("p#show-user").append("<p><span class='new-id-number'>" + newUser.idNumber + "</span></p>");
-    $("p#show-user").append("<p><span class='new-phone-number'>" + newUser.phoneNumber + "</span></p>");
-    $("p#show-user").append("<p><span class='new-email'>" + newUser.email + "</span></p>");
-    $("p#show-user").append("<p><span class='new-address'>" + newUser.address + "</span></p>");
-    $("p#show-user").append("<p><span class='new-post-code'>" + newUser.postCode + "</span></p>");
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-id-number").val("");
-    $("input#new-phone-number").val("");
-    $("input#new-email").val("");
-    $("input#new-address").val("");
-    $("input#new-post-code").val("");
+    /* $("#new-first-name").val("");
+      $("#new-last-name").val("");
+      $("#new-id-number").val("");
+      $("#new-phone-number").val("");
+      $("#new-email").val("");
+      $("#new-address").val("");
+      */
+    $("#submit").last().click(function() {
+      $("#show-user").show();
+      $(".new-first-name").text(newUser.firstName);
+      $(".new-last-name").text(newUser.lastName);
+      $(".new-id-number").text(newUser.idNumber);
+      $(".new-phone-number").text(newUser.phoneNumber);
+      $(".new-email").text(newUser.email);
+      $(".new-address").text(newUser.address);
+      $(".new-post-code").text(newUser.postCode);
 
+    });
+
+console.log(inputtedPhoneNumber);
   });
 });
